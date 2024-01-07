60a97d0fa2a81b59bdf5bcbbcf525d4b3be0d447
muddyrain <qiu3291002845@gmail.com>
2024-01-07 14:26:25 +0800

    feat: -01-07

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"strconv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Query())
-	_, err := w.Write([]byte("check your terminal"))
-	if err != nil {
-		return
-	}
-}
 func main() {
-	http.HandleFunc("/", handler)
+	var a int = 2
+	var b int = 3
+	a, b = swap(a, b)
+	println(a, b)
+}
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		return
+func convertToBin(n int) string {
+	res := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		res = strconv.Itoa(lsb) + res
 	}
+	return res
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
 }
