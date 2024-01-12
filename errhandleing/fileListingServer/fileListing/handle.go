d83f64199d2c5140e60ea8f138c8cfcd08e39b6c
“muddyrain” <qiu3291002845@gmail.com>
2024-01-12 16:47:34 +0800

    fix: 自定义error类型 处理容错

--- errhandleing/fileListingServer/fileListing/handle.go
+++ errhandleing/fileListingServer/fileListing/handle.go
@@ -4,10 +4,26 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const prefix = "/list/"
+
+type UserError string
+
+func (u UserError) Error() string {
+	return u.Message()
+}
+
+func (u UserError) Message() string {
+	return string(u)
+}
+
 func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
-	path := request.URL.Path[len("/list/"):]
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return UserError("path must start with " + prefix)
+	}
+	path := request.URL.Path[len(prefix):]
 	file, err := os.Open(path)
 	if err != nil {
 		//http.Error(writer, err.Error(), http.StatusInternalServerError)
--- errhandleing/fileListingServer/web.go
+++ errhandleing/fileListingServer/web.go
@@ -3,6 +3,7 @@ package main
 import (
 	"awesomeProject/errhandleing/fileListingServer/fileListing"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
@@ -11,9 +12,19 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(w http.ResponseWriter, r *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
 		err := handler(writer, request)
 		if err != nil {
 			fmt.Println("Error handling request: ", err.Error())
+			if userError, ok := err.(userError); ok {
+				http.Error(writer, userError.Message(), http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			if os.IsNotExist(err) {
 				code = http.StatusNotFound
@@ -27,8 +38,13 @@ func errWrapper(handler appHandler) func(w http.ResponseWriter, r *http.Request)
 	}
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(fileListing.HandleFileList))
+	http.HandleFunc("/", errWrapper(fileListing.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
