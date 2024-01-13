23253b101b49d9d0b5020a5c3a28aa41aab104de
muddyrain <qiu3291002845@gmail.com>
2024-01-14 00:58:01 +0800

    feat: pprof 性能优化

--- noneRepeating/noneRepeating.go
+++ noneRepeating/noneRepeating.go
@@ -1,12 +1,16 @@
 package main
 
+var lastOccurred = make([]int, 0xffff)
+
 func lengthOfNoneRepeatingSubStr(s string) int {
-	lastOccurred := make(map[rune]int)
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
 	start := 0
 	maxLength := 0
 	for i, ch := range []rune(s) {
 		// rune 转为 字符串
-		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
 			start = lastI + 1
 		}
 		if i-start+1 > maxLength {
--- noneRepeating/noneRepeating_test.go
+++ noneRepeating/noneRepeating_test.go
@@ -27,7 +27,12 @@ func TestNoneRepeating(t *testing.T) {
 
 func BenchmarkSubStr(b *testing.B) {
 	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s = s + s
+	}
 	ans := 8
+	b.Logf("len(s) = %d", len(s))
+	b.ResetTimer()
 	res := lengthOfNoneRepeatingSubStr(s)
 	for i := 0; i < b.N; i++ {
 		if res != ans {
